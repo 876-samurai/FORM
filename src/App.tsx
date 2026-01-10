9429ffe797d51032c4b4d97d206380daed4c5d91
876-samurai <c.mcintosh876@gmail.com>
2026-01-10 01:41:11 -0800

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { Button } from '@components/common/Button';
 // import { Card } from '@components/common/Card';
 // import { Input } from '@components/common/Input';
-import { LandingPage } from '@pages/LandingPage';
+import { LandingPage } from 'src/pages/LandingPage/LandingPage';
 
 function App() {
   return <LandingPage />;
